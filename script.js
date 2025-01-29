53f055db2e06890d9831cada0d01bd09f5bb9a07
JOSE MATOS <jose_matos@bhd.com.do>
2025-01-29 11:56:11 -0400

    feat: Optimized event listener handlers

--- script.js
+++ script.js
@@ -1,50 +1,65 @@
-// Variables para los elementos del DOM
+//#region  Variables para los elementos del DOM
 const numero1 = document.getElementById("numero1");
 const numero2 = document.getElementById("numero2");
 const sumarBtn = document.getElementById("sumar");
 const restarBtn = document.getElementById("restar");
 const multiplicarBtn = document.getElementById("multi");
+const dividirBtn = document.getElementById("division");
 const resultado = document.getElementById("resultado");
+//#endregion
 
-// Helpers
+//#region  Helpers
 // Funcion para validar la entrada
 function validateInput(number1, number2) {
-    if(isNaN(parseFloat(number1)) || isNaN(parseFloat(number2))) {
-        resultado.textContent = "Error: Ingrese numeros validos e intente de nuevo..."
-    } else {
-      return [parseFloat(number1), parseFloat(number2)];
-    }
+  if (isNaN(parseFloat(number1)) || isNaN(parseFloat(number2))) {
+    resultado.textContent =
+      "Error: Input some valid numbers and try again...";
+  } else {
+    return [parseFloat(number1), parseFloat(number2)];
+  }
 }
-// Funcion para sumar
-function sum(number1, number2) {
-  return number1 + number2;
-}
+//#endregion
 
+//#region  Handlers
 // Función para sumar dos números
-function add() {
-  let numbers = validateInput(numero1.value, numero2.value);
-  const suma = sum(numbers[0], numbers[1]);
-  resultado.textContent = `Resultado: ${suma}`;
+function additionHandler(number1, number2) {
+  let numbers = validateInput(number1, number2);
+  const suma = numbers[0] + numbers[1];
+  resultado.textContent = `Result: ${suma}`;
 }
 // Función para restar dos números
-function substract() {
-  let numbers = validateInput(numero1.value, numero2.value);
+function substractionHandler(number1, number2) {
+  let numbers = validateInput(number1, number2);
   const resta = numbers[0] - numbers[1];
-  resultado.textContent = `Resultado: ${resta}`;
+  resultado.textContent = `Result: ${resta}`;
 }
 
 // Función para multiplicar dos números
-function multiply() {
-  let numbers = validateInput(numero1.value, numero2.value);
-  let result = numbers[0];
-  for (let i = 1; i < numbers[1]; i++) {
-    result = sum(result, numbers[0])
-  }
-  resultado.textContent = `Resultado: ${result}`;
+function multiplicationHandler(number1, number2) {
+  let numbers = validateInput(number1, number2);
+  let result = numbers[0] * numbers[1];
+  resultado.textContent = `Result: ${result}`;
 }
-// Función para dividir dos números
 
-// Event Listeners para botones
-sumarBtn.addEventListener("click", add);
-restarBtn.addEventListener("click", substract);
-multiplicarBtn.addEventListener("click", multiply);
+// Función para dividir dos números
+function divisionHandler(number1, number2) {
+  let numbers = validateInput(number1, number2);
+  let result = numbers[0] / numbers[1];
+  resultado.textContent = `Result: ${result}`;
+}
+//#endregion
+
+//#region  Event Listeners para botones
+sumarBtn.addEventListener("click", () =>
+  additionHandler(numero1.value, numero2.value)
+);
+restarBtn.addEventListener("click", () =>
+  substractionHandler(numero1.value, numero2.value)
+);
+multiplicarBtn.addEventListener("click", () => {
+  multiplicationHandler(numero1.value, numero2.value)
+});
+dividirBtn.addEventListener("click", () => {
+  divisionHandler(numero1.value, numero2.value)
+});
+//#endregion
